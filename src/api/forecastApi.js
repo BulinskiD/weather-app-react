401576dc8ba8473d55e5b9ad0f6716e2c5a90840
BulinskiD <dawidbulinski132@gmail.com>
2019-05-21 23:34:54 +0200

    Forecast Api creator

--- src/api/forecastApi.js
+++ src/api/forecastApi.js
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
     method: 'get',
-    baseURL: 'api.openweathermap.org/data/2.5/appid=abfefee8d62ac2088a5c0ba9d34e1b8c'
+    baseURL: 'https://api.openweathermap.org/data/2.5/forecast',
+    params: {
+        appid: 'abfefee8d62ac2088a5c0ba9d34e1b8c'
+    }
 });
