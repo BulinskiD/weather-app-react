7a381cb9ecb9f7d384475a20aa6cc5f447d4a0f4
BulinskiD <dawidbulinski132@gmail.com>
2019-05-21 22:24:05 +0200

    Added dumb city-adder component

---
+++ src/city-adder/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+export default () => {
+    return (
+        <Row>
+        <InputGroup>
+            <FormControl
+                className="col-8"
+                placeholder="Nazwa miasta"
+                aria-label="city"
+                aria-describedby="basic-addon1"
+            />
+            <Button className="offset-1 col-3" variant="primary"> Dodaj</Button>
+        </InputGroup>
+        </Row>
+    );
+}
