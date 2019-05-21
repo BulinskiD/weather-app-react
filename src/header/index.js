682ba896c6a73dd029b39eac4e4a56bee0db330a
BulinskiD <dawidbulinski132@gmail.com>
2019-05-21 22:23:24 +0200

    Added skeleton components

--- src/header/index.js
+++ src/header/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 
 export default () => {
     return (
-        <div>Navbar</div>
+        <Navbar bg="light" className="my-4 mx-4">
+            <Button className="offset-11 col-1 bg-light text-info">Ustawienia</Button>
+        </Navbar>
     );
 }
