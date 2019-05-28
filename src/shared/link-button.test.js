75015587dc4bc21e12d120ed9b6ca60923c7295d
BulinskiD <dawidbulinski132@gmail.com>
2019-05-28 22:18:52 +0200

    Basic tests of shared components

---
+++ src/shared/link-button.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LinkButton from './link-button';
+import {shallow} from "enzyme";
+
+
+describe('Link button', ()=>{
+
+    it('should render correctly with given props', ()=>{
+        const path= "test";
+        const component = shallow(<LinkButton path={path}>Text</LinkButton>);
+        expect(component).toMatchSnapshot();
+    });
+});
